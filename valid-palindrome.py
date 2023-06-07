3f32661665ac209edfaf582e7f82aa48edbc4351
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-06-06 23:36:18 -0700

    Completed 125. Valid Palindrome

---
+++ valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        left = 0
+        right = len(s) - 1
+
+        while(left < right):
+            if not s[left].isalnum(): #checks to see if alphanumeric, shift if not
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower(): # if inequality, not a palindrome
+                return False
+            else:
+                left += 1
+                right -= 1
+        
+        return True
