81b42016b736ab3b15c7a25b2e26863c5cdf6bd1
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-11-15 00:18:54 -0600

    Completed 274. H-Index

---
+++ h-index.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+
+        citations.sort(reverse=True)
+        n = len(citations)
+        h = 0
+
+        for i in range(n):
+            if citations[i] >= i+1:
+                h = i+1
+
+        return h
